0451573af5ecbecb9e341fe24e6b3651ea1b548e
yuonny <dvkim@ucdavis.edu>
2025-07-15 23:00:22 -0700

    added file

--- pysim.py
+++ pysim.py
@@ -3,8 +3,8 @@ import pybullet as p
 p.connect(p.GUI)
 
 p.setGravity(0, 0, -9.8)
-
 p.loadURDF("combined.urdf", [0,0,0])
+# p.loadURDF("final_rooted_combined_matlab.urdf", [0,0,0])
 
 while True:
     p.stepSimulation()
